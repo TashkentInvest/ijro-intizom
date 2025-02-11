c600415636d1c5f7e358ae43b1820f26beee13f5
InvestUz <invest.abdusattorov@gmail.com>
2025-02-11 12:34:41 +0500

    fixed

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>NobleUI Responsive Bootstrap 4 Dashboard Template</title>
+    <title>EDO</title>
     <!-- core:css -->
     <link rel="stylesheet" href="{{ asset('edo_template/assets/vendors/core/core.css') }}">
     <!-- endinject -->
